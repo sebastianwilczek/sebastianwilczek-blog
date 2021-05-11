c68dd5330c28f34968a81543f8b9f43bdacd1f32
Sebastian Wilczek <sebastian.wilczek@protonmail.com>
2021-05-11 23:03:43 +0200

    Finished style setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Link,
+  Redirect,
   useParams,
+  useLocation,
 } from "react-router-dom";
 
+import * as FakeTime from "./posts/FakeTime";
+
 import "./App.css";
+
+const posts = {
+  fakeTime: FakeTime,
+};
 
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <div className="content">
         <Switch>
           <Route path="/blog/:id">
             <BlogPost />
-          </Route>
-          <Route path="/about">
-            <About />
           </Route>
           <Route path="/">
             <Home />
@@ -28,84 +35,80 @@ const App = () => {
   );
 };
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 const Home = () => {
+  useEffect(() => {
+    document.title = "Sebastian Wilczek, Software Engineer";
+  }, []);
+
   return (
     <>
       <h1 className="homeHeader">{"_> sebastian.wilczek"}</h1>
+      <h2 className="homeSubheader">Software Engineer.</h2>
+      <h3 className="homeEmailHeader">sebastian.wilczek@protonmail.com</h3>
+
+      <h2 className="homeTopicHeader">About</h2>
+      <p className="paragraph">
+        I am a software engineer interested in all things tech, efficiency,
+        healthy life and human relationships. As you can probably imagine, this
+        blog is about all that.
+      </p>
+
+      <h2 className="homeTopicHeader">Blog</h2>
+
+      {Object.keys(posts).length > 0 ? (
+        Object.keys(posts).map((id) => {
+          const Post = posts[id];
+          return (
+            <div className="blogPreview">
+              <Link className="blogPreviewHeader" to={`/blog/${id}`}>
+                {Post.title}
+              </Link>
+              <p className="blogPreviewInfo">
+                <i>{Post.date}</i>
+              </p>
+              <Post.Preview />
+            </div>
+          );
+        })
+      ) : (
+        <p className="paragraph">Coming soon.</p>
+      )}
     </>
   );
 };
 
-const About = () => {
-  return <h2>About</h2>;
-};
-
 const BlogPost = () => {
   let { id } = useParams();
+  const Post = posts[id];
+
+  useEffect(() => {
+    document.title = `${Post?.title} - Sebastian Wilczek, Software Engineer`;
+  }, [Post]);
+
+  if (!Post) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <>
-      <h1 className="header">Fake time</h1>
+      <Link className="goBack" to="/">
+        {"< Go back"}
+      </Link>
+      <h1 className="header">{Post.title}</h1>
       <p className="info">
-        by Sebastian Wilczek - <i>11.05.2021</i>
+        by Sebastian Wilczek - <i>{Post.date}</i>
       </p>
-      <p className="paragraph">
-        We are always supposed to make time. Time for our jobs, our hobbies,
-        friends, family, walking the dog, cleaning up. We are supposed to make
-        time to relax. How ironic.
-      </p>
-      <p className="paragraph">
-        As many of you can probably relate, I have spent a reasonable amount of
-        time just managing my time. A meeting here, an appointment there, and
-        make sure to cram your relationships in there as well! I often found
-        myself in a constant battle for and against time. Given my passion for
-        tinkering with all things tech, as well as also trying to lead something
-        that resembles a somewhat healthy lifestyle, different priorities often
-        competed with each other for my precious time. The aftermath usually
-        looked like my calender became a Jackson Pollock painting. I needed to
-        optimize.
-      </p>
-      <p className="paragraph">
-        Of course, the Internet has no shortage of guides and tips to manage
-        time. My personal favourite actually comes straight from the hands of
-        two people who helped create some of our biggest time drains: Jake Knapp
-        and John Zeratsky. Before they wrote the book that made me rethink time
-        and the activities I pursue, they worked for Google, and, while doing
-        so, created software that you probably use on the regular: YouTube and
-        Gmail. If anyone knows anything about time and how you spend it, they
-        would surely make the list.
-      </p>
-      <p className="paragraph">
-        They condensed their thoughts and, more importantly, their actions
-        regarding time into one book: <i>"Make time"</i>. If I were to put it
-        into one sentence (for the sake of time), I'd describe it as{" "}
-        <i>"a collection of actionable advice to get shit done"</i>. However,
-        this short condensation is not accurate. If anything, the book tells you
-        to not get as much shit done as you may think. One of the main things
-        the book refers to is the <i>"busy bandwagon"</i>, the idea that being
-        busy all the time at 100% is a really good thing. Spoiler: It is not.
-      </p>
-      <p className="paragraph">
-        As mentioned, the book basically has a list of things to do to make room
-        in your day/week/life for the things you want to do. It makes you make
-        time. One particular advice stood out to me: the idea of a daily
-        highlight. As I am writing this, I am also eating a bucket of ice cream.
-        While satisfying, it is not my highlight of the day. What the authors
-        refer to when mentioning a 'highlight' is, in simple terms, the thing
-        you want to do today. This may sound obvious, but do you really focus on
-        one thing each day? One thing you really want to do? The answers is
-        likely no, unless you followed the authors advice or a similar strategy.
-        The main idea is:{" "}
-        <i>
-          "If you do a daily highlight every day, after a year you will have
-          done 365 things that mattered."
-        </i>
-      </p>
-      <p className="paragraph">
-        When I started "making time" in form of a daily highlight, I was also
-        highly sceptical. Of course I could find a thing to do each day that I
-        would really like to do. Hell, I could find 365 things right then and
-        there!
-      </p>
+      <Post.Content />
     </>
   );
 };
