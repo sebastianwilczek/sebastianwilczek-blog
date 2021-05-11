2b814a7c75997f5f3af8b515dfa34e5e739d6782
Sebastian Wilczek <sebastian.wilczek@protonmail.com>
2021-05-11 19:56:33 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams,
+} from "react-router-dom";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="content">
+        <Switch>
+          <Route path="/blog/:id">
+            <BlogPost />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="homeHeader">{"_> sebastian.wilczek"}</h1>
+    </>
+  );
+};
+
+const About = () => {
+  return <h2>About</h2>;
+};
+
+const BlogPost = () => {
+  let { id } = useParams();
+  return (
+    <>
+      <h1 className="header">Fake time</h1>
+      <p className="info">
+        by Sebastian Wilczek - <i>11.05.2021</i>
+      </p>
+      <p className="paragraph">
+        We are always supposed to make time. Time for our jobs, our hobbies,
+        friends, family, walking the dog, cleaning up. We are supposed to make
+        time to relax. How ironic.
+      </p>
+      <p className="paragraph">
+        As many of you can probably relate, I have spent a reasonable amount of
+        time just managing my time. A meeting here, an appointment there, and
+        make sure to cram your relationships in there as well! I often found
+        myself in a constant battle for and against time. Given my passion for
+        tinkering with all things tech, as well as also trying to lead something
+        that resembles a somewhat healthy lifestyle, different priorities often
+        competed with each other for my precious time. The aftermath usually
+        looked like my calender became a Jackson Pollock painting. I needed to
+        optimize.
+      </p>
+      <p className="paragraph">
+        Of course, the Internet has no shortage of guides and tips to manage
+        time. My personal favourite actually comes straight from the hands of
+        two people who helped create some of our biggest time drains: Jake Knapp
+        and John Zeratsky. Before they wrote the book that made me rethink time
+        and the activities I pursue, they worked for Google, and, while doing
+        so, created software that you probably use on the regular: YouTube and
+        Gmail. If anyone knows anything about time and how you spend it, they
+        would surely make the list.
+      </p>
+      <p className="paragraph">
+        They condensed their thoughts and, more importantly, their actions
+        regarding time into one book: <i>"Make time"</i>. If I were to put it
+        into one sentence (for the sake of time), I'd describe it as{" "}
+        <i>"a collection of actionable advice to get shit done"</i>. However,
+        this short condensation is not accurate. If anything, the book tells you
+        to not get as much shit done as you may think. One of the main things
+        the book refers to is the <i>"busy bandwagon"</i>, the idea that being
+        busy all the time at 100% is a really good thing. Spoiler: It is not.
+      </p>
+      <p className="paragraph">
+        As mentioned, the book basically has a list of things to do to make room
+        in your day/week/life for the things you want to do. It makes you make
+        time. One particular advice stood out to me: the idea of a daily
+        highlight. As I am writing this, I am also eating a bucket of ice cream.
+        While satisfying, it is not my highlight of the day. What the authors
+        refer to when mentioning a 'highlight' is, in simple terms, the thing
+        you want to do today. This may sound obvious, but do you really focus on
+        one thing each day? One thing you really want to do? The answers is
+        likely no, unless you followed the authors advice or a similar strategy.
+        The main idea is:{" "}
+        <i>
+          "If you do a daily highlight every day, after a year you will have
+          done 365 things that mattered."
+        </i>
+      </p>
+      <p className="paragraph">
+        When I started "making time" in form of a daily highlight, I was also
+        highly sceptical. Of course I could find a thing to do each day that I
+        would really like to do. Hell, I could find 365 things right then and
+        there!
+      </p>
+    </>
+  );
+};
+
+export default App;
