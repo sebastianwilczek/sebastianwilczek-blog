e7855a93e83cc97368109c23a1764bfe92d31035
Sebastian Wilczek <sebastian.wilczek@protonmail.com>
2021-05-13 11:19:50 +0200

    Proof reading Fake time

--- src/posts/FakeTime.js
+++ src/posts/FakeTime.js
@@ -29,16 +29,16 @@ export const Content = () => {
         tinkering with all things tech, as well as also trying to lead something
         that resembles a somewhat healthy lifestyle, different priorities often
         competed with each other for my precious time. The aftermath usually
-        looked like my calender became a Jackson Pollock painting. I needed to
+        looked like my calendar became a Jackson Pollock painting. I needed to
         optimize.
       </p>
       <p className="paragraph">
         Of course, the Internet has no shortage of guides and tips to manage
-        time. My personal favourite actually comes straight from the hands of
-        two people who helped create some of our biggest time drains: Jake Knapp
-        and John Zeratsky. Before they wrote the book that made me rethink time
-        and the activities I pursue, they worked for Google, and, while doing
-        so, created software that you probably use on the regular: YouTube and
+        time. My personal favorite comes straight from the hands of two people
+        who helped create some of our biggest time drains, Jake Knapp and John
+        Zeratsky. Before they wrote the book that made me rethink time and the
+        activities I pursue, they worked for Google, and, while doing so,
+        created software that you probably use on the regular: YouTube and
         Gmail. If anyone knows anything about time and how you spend it, they
         would surely make the list.
       </p>
@@ -53,16 +53,16 @@ export const Content = () => {
         busy all the time at 100% is a really good thing. Spoiler: It is not.
       </p>
       <p className="paragraph">
-        As mentioned, the book basically has a list of things to do to make room
-        in your day/week/life for the things you want to do. It makes you make
-        time. One particular advice is heavily mentioned by the authors and
-        stood out to me: the idea of a daily highlight. As I am writing this, I
-        am also eating a bucket of ice cream. While satisfying, it is not my
+        As mentioned, the book has a list of things to do to make room in your
+        day/week/life for the things you want to do. It makes you make time. One
+        particular piece of advice is heavily mentioned by the authors and stood
+        out to me: the idea of a daily highlight. As I am writing this, I am
+        also eating a bucket of ice cream. While satisfying, it is not my
         highlight of the day. What the authors refer to when mentioning a
         'highlight' is, in simple terms, the thing you want to do today. This
-        may sound obvious, but do you really focus on one thing each day? One
-        thing you really want to do? The answers is likely no, unless you
-        followed the authors advice or a similar strategy. The main idea is:{" "}
+        may sound obvious, but do you focus on one thing each day? One thing you
+        want to do? The answer is likely no unless you followed the authors'
+        advice or a similar strategy. The main idea is:{" "}
         <i>
           "If you do a daily highlight every day, after a year you will have
           done at least 365 things that mattered."
@@ -70,25 +70,25 @@ export const Content = () => {
       </p>
       <p className="paragraph">
         When I started "making time" in form of a daily highlight, I was also
-        highly sceptical. Of course I could find a thing to do each day that I
-        would really like to do. Hell, I could find 365 things right then and
-        there! My problem was not that I did not know what I wanted to do, my
-        problem was that I had way too many different things to do that were in
-        the way of me enjoying my highlight, or the activities I enjoy in
-        general. That much was obvious, or at least I thought so.
+        highly skeptical. Of course, I could find a thing to do each day that I
+        would like to do. Hell, I could find 365 things right then and there! My
+        problem was not that I did not know what I wanted to do, my problem was
+        that I had way too many different things to do that were in the way of
+        me enjoying my highlight, or the activities I enjoy in general. That
+        much was obvious, or at least I thought so.
       </p>
       <p className="paragraph">
         Nonetheless, I gave it my best shot. For a while, every day, I marked an
         event in my calendar to be the designated highlight of the day. The
         activities themselves were rather varied, encompassing things such as
         trying out a new recipe to cook, implementing a feature in one of my
-        projects or spending time doing some activities with my girlfriend or
+        projects, or spending time doing some activities with my girlfriend or
         perhaps one or more good friends. And while the activities themselves
         crossed a large range of topics, there were a few common properties all
         highlights shared. Usually, I planned around 1-2 hours for the
         highlight, and I placed it sometime in the afternoon or evening. If
         something else came up, I always attempted to make my highlight the
-        priority, rescheduling or cancelling whatever else may happen at the
+        priority, rescheduling or canceling whatever else may happen at the
         time. And I always marked the daily highlight in a{" "}
         <span style={{ color: "red" }}>bright red</span> in my schedule.
       </p>
@@ -100,9 +100,9 @@ export const Content = () => {
         to the millisecond for the sake of science. Gut-feeling: I spent more
         time with things I enjoy. However, there was a lingering thought
         remaining. I am only spending time with things I would have likely done
-        in a similar manner, I am just mindful that I am doing them every day.
-        Some tasks are still not in my focus, and my attention to them may even
-        suffer due to me just focusing on the good stuff.
+        similarly, I am just mindful that I am doing them every day. Some tasks
+        are still not in my focus, and my attention to them may even suffer due
+        to me just focusing on the good stuff.
       </p>
       <p className="paragraph">And then, something peculiar happened.</p>
       <p className="paragraph">
@@ -116,7 +116,7 @@ export const Content = () => {
         </span>
       </p>
       <p className="paragraph">
-        At this point you may be asking yourself: why does this guy enjoy
+        At this point, you may be asking yourself: why does this guy enjoy
         washing his car so much that he dedicated the peak point of his day to
         doing so? Sure, there are certainly people who are crazy about their
         cars. Perhaps even so much so that polishing their rims is equivalent
@@ -130,22 +130,21 @@ export const Content = () => {
         I did not think about any of that when I marked it in my calendar. I
         said the night before that I would like to wash my car, and that I
         should do it as a highlight. Intrusive thoughts, such as{" "}
-        <i>"Is this really something I enjoy?"</i>, did not even come up. I
-        scheduled it, I even looked forward to it. I relished in taking care of
-        that vehicle. I enjoyed dusting off the dashboard to make it nice and
-        shiny. I even took a step back at the end to admire how clean this small
-        car can look.
+        <i>"Is this something I enjoy?"</i>, did not even come up. I scheduled
+        it, I even looked forward to it. I relished taking care of that vehicle.
+        I enjoyed dusting off the dashboard to make it nice and shiny. I even
+        took a step back at the end to admire how clean this small car can look.
       </p>
       <p className="paragraph">
         It was only at the end of the day that I realized what I had done.
         Looking back at my schedule, I noticed that I did not pretend to enjoy
-        washing the car. It actually became my daily highlight, through the
-        sheer action of defining it as such. Setting a car-cleaning highlight of
-        the day triggered a chain reaction that seeems to have taken root in my
-        habits before. Up to this point, I limited my daily highlights to things
-        I knew I would enjoy. Those were those beautiful red spots in the late
-        parts of my schedule that would give me that energy boost or that sense
-        of fulfillment.
+        washing the car. It became my daily highlight, through the sheer act of
+        defining it as such. Setting a car-cleaning highlight of the day
+        triggered a chain reaction that seems to have taken root in my habits
+        before. Up to this point, I limited my daily highlights to things I knew
+        I would enjoy. Those were those beautiful red spots in the late parts of
+        my schedule that would give me that energy boost or that sense of
+        fulfillment.
       </p>
       <p className="paragraph">
         And that is when it hit me: By setting a daily highlight, I had trained
@@ -153,29 +152,29 @@ export const Content = () => {
         the reward that was the activity itself. Keep in mind, those highlights
         were things I enjoy, always. I enforced and reinforced the idea that the
         daily highlight is the peak point of enjoyment every day. And then,
-        simply by marking a mundane tasks in a bright red, my brain assumed it
-        to be a good thing. Something to focus on. An activity to store in the{" "}
+        simply by marking a mundane task in a bright red, my brain assumed it to
+        be a good thing. Something to focus on. An activity to store in the{" "}
         <i>"Oh boy, you are so looking forward to this"</i> folder. Only giving
         something as tiring as washing your car a label I had used for a while,
-        it gave me that dopamine and serotonine rush that we all desire.
+        gave me that dopamine and serotonin rush that we all desire.
       </p>
       <p className="paragraph">
         I had transformed a banal task into something I latched on to for
         enjoyment, just for that one day. I had created <i>"fake time"</i>, a
-        part of my day that needed to be the way it was and that I tricked my
+        part of my day that needed to be the way it was, and that I tricked my
         brain into believing it was the best thing ever.
       </p>
       <p className="paragraph">
-        Now, if I were to figuratively wash my car everyday, this effect would
-        likely cease to exist. The amazing thing would become the great, would
-        become the normal, would become the boring. And ideally, this is
-        probably how it should be. You do not want your highlight to be become
-        things that you have to do only. That's not a highlight! I keep on using
-        my highlight for things that bring me enjoyment or progress. However,
-        the idea that a simple task on my To-Do list could be moved to the high
-        spot without me even realizing it to be boring offers the possibility of
-        getting those things done over time, which in turn makes time for more
-        highlights. I was sceptical, but simply trying proved me wrong.
+        Now, if I were to figuratively wash my car every day, this effect would
+        likely cease to exist. The amazing thing would become great, would
+        become normal, would become boring. And ideally, this is probably how it
+        should be. You do not want your highlight to become things that you have
+        to do only. That's not a highlight! I keep on using my highlight for
+        things that bring me enjoyment or progress. However, the idea that a
+        simple task on my To-Do list could be moved to the high spot without me
+        even realizing it to be boring offers the possibility of getting those
+        things done over time, which in turn makes time for more highlights. I
+        was skeptical, but simply trying proved me wrong.
       </p>
       <p className="paragraph">
         What can you take away from my little self-experiment? First of all, I
@@ -191,9 +190,9 @@ export const Content = () => {
         <i>"Make time"</i>. I have yet to try all the strategies to create more
         time in your life myself, and if they all have a similar impact to what
         has been detailed here, they surely are... well, worth your time. Up
-        until then, I myself will continue adding highlights to each of my days.
-        But occassionly, I might garnish it with something boring but essential
-        and unexpected. A sprinkle of <i>"fake time"</i>.
+        until then, I will continue adding highlights to each of my days. But
+        occasionally, I might garnish it with something boring but essential and
+        unexpected. A sprinkle of <i>"fake time"</i>.
       </p>
     </>
   );
